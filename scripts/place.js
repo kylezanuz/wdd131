503b3aa3ae62b50babdb327afca91bd1df253df2
kylezanuz <kylesanzanuz@gmail.com>
2026-01-30 01:43:22 -0800

    updated-place-w03

--- scripts/place.js
+++ scripts/place.js
@@ -4,6 +4,7 @@ document.getElementById('modified').textContent = document.lastModified;
 const temperature = 8;
 const windSpeed = 12;
 
+
 function calculateWindChill(t, s) {
     return 13.12 + 0.6215 * t - 11.37 * Math.pow(s, 0.16) + 0.3965 * t * Math.pow(s, 0.16);
 }
